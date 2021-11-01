a7e2a83e73bff3fbb57ca4a17f120c4429e11db1
bstmrc <mbasta.1402@gmail.com>
2021-11-01 13:46:11 -0300

    add gen_binario() feature

--- modulo.py
+++ modulo.py
@@ -4,3 +4,82 @@ from Registro import *
 import pickle
 import os.path
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def gen_binario(i, vec):
+
+    fd = 'MusicaIdioma' + str(i) + '.dat'
+    
+    m = open(fd, 'wb')
+
+    for item in vec:
+        if item.idioma == i:
+            pickle.dump(i, m)
+            pickle.dump('\n', m)
+
+    m.close()
+
+
